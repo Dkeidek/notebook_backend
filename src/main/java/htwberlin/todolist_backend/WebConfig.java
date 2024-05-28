77bd59d11d7284a638e694c2d1b02ed9a56f2903
mohdk <Mohammad.Dkeidek@Student.HTW-Berlin.de>
2024-05-28 15:11:34 +0200

    CORS configuration

---
+++ src/main/java/htwberlin/todolist_backend/WebConfig.java
@@ -0,0 +1,19 @@
+package htwberlin.todolist_backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost:5177", "https://notebook-frontend-pl4e.onrender.com")
+                .allowCredentials(true);
+    }
+}
